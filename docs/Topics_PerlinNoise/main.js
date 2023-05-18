862f9d52196ffc01253c406d9cc63f0b79f48af6
[eugenio] <geniosainz@gmail.com>
2023-05-18 23:51:33 +0200

    Perlin Noise

--- docs/Topics_PerlinNoise/main.js
+++ docs/Topics_PerlinNoise/main.js
@@ -45,7 +45,7 @@ function calcNoise(){
         
         noiseArray.push({x,y})
 
-        t+=0.01
+        t+=0.02
 
        };
 
@@ -57,3 +57,31 @@ function windowResized(){
     resizeCanvas(windowWidth, windowHeight);
 };
 
+var x = 1.6;
+var y = 1.4;
+
+var  X = Math.floor(x) & 255;	// Used later
+var  Y = Math.floor(y) & 255;	// Used later
+var  xf = x-Math.floor(x);
+var  yf = y-Math.floor(y);
+var  topRight    = new p5.Vector(xf-1.0, yf-1.0);
+var  topLeft     = new p5.Vector(xf, yf-1.0);
+var  bottomRight = new p5.Vector(xf-1.0, yf);
+var  bottomLeft  = new p5.Vector(xf, yf);
+
+console.log({xf,yf,topRight,topLeft,bottomRight, bottomLeft})
+
+topRight    = [ xf-1.0, yf-1.0];
+topLeft     = [ xf, yf-1.0];
+bottomRight = [ xf-1.0, yf];
+bottomLeft  = [ xf, yf];
+
+valueTopRight    = permutation( permutation( X+1) + Y+1);
+valueTopLeft     = permutation( permutation( X  ) + Y+1);
+valueBottomRight = permutation( permutation( X+1) + Y  );
+valueBottomLeft  = permutation( permutation( X  ) + Y  );
+
+dotTopRight    = dot( topRight   , GetConstantVector(valueTopRight));
+dotTopLeft     = dot( topLeft    , GetConstantVector(valueTopLeft));
+dotBottomRight = dot( bottomRight, GetConstantVector(valueBottomRight));
+dotBottomLeft  = dot( bottomLeft , GetConstantVector(valueBottomLeft));
